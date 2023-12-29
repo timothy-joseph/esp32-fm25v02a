d70d62cd764ae688fe1ead0befec257061ba6b12
Timothy Joseph <timoteicicu1122@gmail.com>
2023-12-29 15:19:24 +0200

    Updated driver
    updated fm25v02.h
    updated fm25v02.c

--- components/fm25v02a/fm25v02a.c
+++ components/fm25v02a/fm25v02a.c
@@ -1,1 +1,102 @@
-#include "fm25v02a.h"
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+
+#include "include/fm25v02a.h"
+
+#define FREQ_MAX 40000000
+
+static void set_cs(spi_transaction_t *t);
+static void clear_cs(spi_transaction_t *t);
+
+esp_err_t
+fram_init(fram_device_t *ret, spi_host_device_t host, gpio_num_t cs,
+	  uint32_t freq)
+{
+	/* TODO: probably not right */
+	spi_device_interface_config_t spi_dev_cfg = {
+		.command_bits = 8,
+		.address_bits = 16,
+		.dummy_bits = 0,
+		.mode = 0,
+		.clock_source = SPI_CLK_SRC_DEFAULT,
+		.clock_speed_hz = freq,
+		.spics_io_num = -1,
+		.pre_cb = clear_cs,
+		.post_cb = set_cs
+	};
+	esp_err_t err;
+
+	if (ret == NULL || host == SPI1_HOST || freq > FREQ_MAX)
+		return ESP_ERR_INVALID_ARG;
+	
+	ret->host = host;
+	ret->cs = cs;
+	ret->use_interrupt = 0;
+
+	err = spi_bus_add_device(host, &spi_dev_cfg, &ret->spi_dev);
+	if (err != ESP_OK)
+		return err;
+	
+	gpio_set_level(cs, 1);
+	gpio_set_direction(cs, GPIO_MODE_OUTPUT);
+
+	return ESP_OK;
+}
+
+esp_err_t
+fram_write_enable(fram_device_t *dev)
+{
+}
+
+esp_err_t
+fram_write_disable(fram_device_t *dev)
+{
+}
+
+esp_err_t
+fram_read_status_register(fram_device_t *dev)
+{
+}
+
+esp_err_t
+fram_write_status_register(fram_device_t *dev, uint8_t force_enable)
+{
+}
+
+esp_err_t
+fram_read(fram_device_t *dev, uint16_t addr, uint8_t *data,
+	  size_t len)
+{
+}
+
+esp_err_t
+fram_fast_read(fram_device_t *dev, uint16_t addr, uint8_t *data,
+	       size_t len)
+{
+}
+
+esp_err_t
+fram_write(fram_device_t *dev, uint16_t addr, uint8_t *data, size_t len,
+	   uint8_t force_enable)
+{
+}
+
+esp_err_t
+fram_sleep(fram_device_t *dev)
+{
+}
+
+esp_err_t
+fram_read_device_id(fram_device_t *dev)
+{
+}
+
+static void
+set_cs(spi_transaction_t *t)
+{
+}
+
+static void
+clear_cs(spi_transaction_t *t)
+{
+}
--- components/fm25v02a/include/fm25v02a.h
+++ components/fm25v02a/include/fm25v02a.h
@@ -1,4 +1,56 @@
 #ifndef FM25V02A_H
 #define FM25V02A_H
 
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+
+/*
+ * macros used by the user and inside of the driver
+ * example:
+ *	if (fram_read_status_register(...) & STATUS_WEL_MASK)
+ *		do_something();
+ *
+ * these indicate the bits used inside of the status register
+ */
+#define STATUS_WEL_MASK 0x02
+#define STATUS_BLOCK_PROTECTION_MASK 0x0c
+#define STATUS_WPEN_MASK 0x80
+
+/*
+ * macros used by the driver to determine to how many bits to shift the input
+ * given by the user for the functions fram_set_status_bp, fram_set_status_wel,
+ * and fram_set_status_wpen. and to determine by how many bits to shift the
+ * output from the chip when the user calls the read-equivalent of those
+ * functions
+ *
+ * the functions described here are TODO
+ */
+#define STATUS_WEL_OFFSET 0
+#define STATUS_BLOCK_PROTECTION_OFFSET 2
+#define STATUS_WPEN_OFFSET 7
+
+/* TODO: add interrupt support */
+typedef struct fram_device fram_device_t;
+struct {
+	spi_host_device_t host;
+	gpio_num_t cs;
+	uint8_t use_interrupt;
+	spi_device_handle_t spi_dev;
+} fram_device;
+
+esp_err_t fram_init(fram_device_t *ret, spi_host_device_t host, gpio_num_t cs,
+		    uint32_t freq);
+esp_err_t fram_write_enable(fram_device_t *dev);
+esp_err_t fram_write_disable(fram_device_t *dev);
+esp_err_t fram_read_status_register(fram_device_t *dev);
+esp_err_t fram_write_status_register(fram_device_t *dev, uint8_t force_enable);
+esp_err_t fram_read(fram_device_t *dev, uint16_t addr, uint8_t *data,
+		    size_t len);
+esp_err_t fram_fast_read(fram_device_t *dev, uint16_t addr, uint8_t *data,
+			 size_t len);
+esp_err_t fram_write(fram_device_t *dev, uint16_t addr, uint8_t *data, size_t len,
+		     uint8_t force_enable);
+esp_err_t fram_sleep(fram_device_t *dev);
+esp_err_t fram_read_device_id(fram_device_t *dev);
+
 #endif /* #ifndef FM25V02A_H */
